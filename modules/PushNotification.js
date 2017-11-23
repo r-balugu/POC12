d1c79948b41c9e0a6d3eb96e1fa798d06cf8db23
RajuBalugu <raju.balugu@kony.com>
2017-11-23 14:31:59 +0530

    Code modulirization

--- modules/PushNotification.js
+++ modules/PushNotification.js
@@ -1,84 +1,99 @@
 gblIDID = null;
 gblDeviceInfo = kony.os.deviceInfo();
 
-
+/**
+ * Purpose: This function is to register all callback's of pushnotification.
+ * 
+ */
 function registerPushCallBacks() {
-  //loginPostShow();
-  try{
-    //initializeMF();
-    notificationKey = kony.store.getItem("NotificationFlag");
-    kony.print("notificationKey ===>" + notificationKey);
-    if (notificationKey == null || notificationKey == true) {
-      notificationKey = true;
-      kony.store.setItem("NotificationFlag", true);
-      kony.push.setCallbacks({
-        onsuccessfulregistration: onSuccessfulRegisration,
-        onfailureregistration: onFailureRegistration,
-        onlinenotification: onlineNotification,
-        offlinenotification: offlineNotification,
-        onsuccessfulderegistration: onSuccessfulDeregistration,
-        onfailurederegistration: onFailureDeregistration
-      });
-      registerAppWithPushNotificationsInfrastructure();
-    } else {
-      notificationKey = false;
-      kony.store.setItem("NotificationFlag", false);
-      kony.print("Do nothing-->> Notifications are turned off by the User");
+    try {
+        notificationKey = kony.store.getItem("NotificationFlag");
+        kony.print("notificationKey ===>" + notificationKey);
+        if (notificationKey === null || notificationKey === true) {
+            notificationKey = true;
+            kony.store.setItem("NotificationFlag", true);
+            kony.push.setCallbacks({
+                onsuccessfulregistration: onSuccessfulRegisration,
+                onfailureregistration: onFailureRegistration,
+                onlinenotification: onlineNotification,
+                offlinenotification: offlineNotification,
+                onsuccessfulderegistration: onSuccessfulDeregistration,
+                onfailurederegistration: onFailureDeregistration
+            });
+            registerAppWithPushNotificationsInfrastructure();
+        } else {
+            notificationKey = false;
+            kony.store.setItem("NotificationFlag", false);
+            kony.print("Do nothing-->> Notifications are turned off by the User");
+        }
+    } catch (e) {
+        kony.print("Failed to register callback functions.");
     }
-  }catch(e){
-    
-  }
 }
 
+/**
+ * Purpose: This function is to prepare the required infrastructure to push messages.
+ * 
+ */
 function registerAppWithPushNotificationsInfrastructure() {
-  try {
-    //#ifdef android
-    config = {
-      senderid: "581331337956"           
-    };
-    //#endif
-    //#ifdef tabrcandroid
-    config = {
-      senderid: "581331337956"           
-    };
-    //#endif
-    //#ifdef iphone
-    config = [0,1,2];
-    //#endif
-    kony.push.register(config);
-  } catch (err) {
-    //Call Error Handler
-    
-  }
+    try {
+        //#ifdef android
+        config = {
+            senderid: "581331337956"
+        };
+        //#endif
+        //#ifdef tabrcandroid
+        config = {
+            senderid: "581331337956"
+        };
+        //#endif
+        //#ifdef iphone
+        config = [0, 1, 2];
+        //#endif
+        kony.push.register(config);
+    } catch (err) {
+        //Call Error Handler
+
+    }
 }
+
+/**
+ * Purpose: This function to subscribe the pushnotification with Kony Messaging service.
+ * 
+ */
 
 function onSuccessfulRegisration(regId) {
-  kony.print("in success reg::==== " + regId);
-  var basicConf = {
-    message: "Registration Successful: RegisrationID: " + regId,
-    alertType: constants.ALERT_TYPE_INFO,
-    alertTitle: "",
-    yesLabel: "OK",
-    noLabel: "",
-    alertHandler: null
-  }
-  var pspConf = {}
-  gPushRegId = regId;
-  subscribeKPNS();
+    kony.print("in success reg::==== " + regId);
+    var basicConf = {
+        message: "Registration Successful: RegisrationID: " + regId,
+        alertType: constants.ALERT_TYPE_INFO,
+        alertTitle: "",
+        yesLabel: "OK",
+        noLabel: "",
+        alertHandler: null
+    };
+    var pspConf = {};
+    gPushRegId = regId;
+    subscribeKPNS();
 }
 
+/**
+ * Purpose: Failure callback of push registration.
+ * 
+ */
+
 function onFailureRegistration(msg) {
-  kony.print("in failure reg::" + JSON.stringify(msg));
-  var basicConf = {
-    message: "Registration Failure: Registration Failure Message: " + msg,
-    alertType: constants.ALERT_TYPE_INFO,
-    alertTitle: "",
-    yesLabel: "OK",
-    noLabel: "",
-    alertHandler: null
-  }
-  var pspConf = {}
-  gPushRegId = "";
+    kony.print("in failure reg::" + JSON.stringify(msg));
+    var basicConf = {
+        message: "Registration Failure: Registration Failure Message: " + msg,
+        alertType: constants.ALERT_TYPE_INFO,
+        alertTitle: "",
+        yesLabel: "OK",
+        noLabel: "",
+        alertHandler: null
+    };
+    var pspConf = {};
+    gPushRegId = "";
 }
 
 /*
@@ -86,189 +101,189 @@ function onFailureRegistration(msg) {
  * 'payload' is an Object that contains a set of key-value pairs provided by the respective Push Notification Server
  */
 function onlineNotification(payload) {
-  kony.print("inside onlineNotification payload is:;" + payload);
-  var title ;
-  var Noticontent ;
-  kony.print("title-->"+payload.title);
+    kony.print("inside onlineNotification payload is:;" + payload);
+    var title;
+    var Noticontent;
+    kony.print("title-->" + payload.title);
 
-  //#ifdef android
-  title = payload["gcm.notification.title"];
-  //#endif       
-  //#ifdef iphone
-  title = payload["alert"]["title"];
-  //#endif
-  //#ifdef tabrcandroid
-  title = payload["gcm.notification.title"];
-  //#endif
-  //#ifdef ipad
-  title = payload["alert"]["title"];
-  //#endif
-
-
-  //#ifdef android
-  Noticontent = payload["gcm.notification.body"];
-  //#endif       
-  //#ifdef iphone
-  Noticontent = payload["alert"]["body"];
-  //#endif
-  //#ifdef tabrcandroid
-  Noticontent = payload["gcm.notification.body"];
-  //#endif
-  //#ifdef ipad
-  Noticontent = payload["alert"]["body"];
-  //#endif
+    //#ifdef android
+    title = payload["gcm.notification.title"];
+    //#endif       
+    //#ifdef iphone
+    title = payload["alert"]["title"];
+    //#endif
+    //#ifdef tabrcandroid
+    title = payload["gcm.notification.title"];
+    //#endif
+    //#ifdef ipad
+    title = payload["alert"]["title"];
+    //#endif
 
 
-  kony.print("title is::" + title);
-  kony.print("content is::" + Noticontent);
-  gblTitleNotification = title;
-  gblContentNotification = Noticontent;
-  kony.print("Received online Notification"); 
-  kony.ui.Alert({
+    //#ifdef android
+    Noticontent = payload["gcm.notification.body"];
+    //#endif       
+    //#ifdef iphone
+    Noticontent = payload["alert"]["body"];
+    //#endif
+    //#ifdef tabrcandroid
+    Noticontent = payload["gcm.notification.body"];
+    //#endif
+    //#ifdef ipad
+    Noticontent = payload["alert"]["body"];
+    //#endif
+
+
+    kony.print("title is::" + title);
+    kony.print("content is::" + Noticontent);
+    gblTitleNotification = title;
+    gblContentNotification = Noticontent;
+    kony.print("Received online Notification");
+    kony.ui.Alert({
         message: gblContentNotification,
-    	alertTitle: gblTitleNotification,
+        alertTitle: gblTitleNotification,
         alertType: constants.ALERT_TYPE_INFO,
         yesLabel: "Ok"
-      }, {});
+    }, {});
 }
 
 
 function offlineNotification(payload) {
-  var title = payload.title;
-  var Noticontent = payload.content;
+    var title = payload.title;
+    var Noticontent = payload.content;
 
-  //#ifdef android
-  title = payload["gcm.notification.title"];
-  //#endif       
-  //#ifdef iphone
-  title = payload["alert"]["title"];
-  //#endif
-  //#ifdef tabrcandroid
-  title = payload["gcm.notification.title"];
-  //#endif
-  //#ifdef ipad
-  title = payload["alert"]["title"];
-  //#endif
+    //#ifdef android
+    title = payload["gcm.notification.title"];
+    //#endif       
+    //#ifdef iphone
+    title = payload["alert"]["title"];
+    //#endif
+    //#ifdef tabrcandroid
+    title = payload["gcm.notification.title"];
+    //#endif
+    //#ifdef ipad
+    title = payload["alert"]["title"];
+    //#endif
 
 
-  //#ifdef android
-  Noticontent = payload["gcm.notification.body"];
-  //#endif       
-  //#ifdef iphone
-  Noticontent = payload["alert"]["body"];
-  //#endif
-  //#ifdef tabrcandroid
-  Noticontent = payload["gcm.notification.body"];
-  //#endif
-  //#ifdef ipad
-  Noticontent = payload["alert"]["body"];
-  //#endif
+    //#ifdef android
+    Noticontent = payload["gcm.notification.body"];
+    //#endif       
+    //#ifdef iphone
+    Noticontent = payload["alert"]["body"];
+    //#endif
+    //#ifdef tabrcandroid
+    Noticontent = payload["gcm.notification.body"];
+    //#endif
+    //#ifdef ipad
+    Noticontent = payload["alert"]["body"];
+    //#endif
 
-  kony.print("title is::" + title);
-  kony.print("content is::" + Noticontent);
+    kony.print("title is::" + title);
+    kony.print("content is::" + Noticontent);
 
-  gblTitleNotification = title;
-  gblContentNotification = Noticontent;
+    gblTitleNotification = title;
+    gblContentNotification = Noticontent;
 
-  alert('offline ' + gblContentNotification);
-  //NotificationMFInitilize();
+    alert('offline ' + gblContentNotification);
+    //NotificationMFInitilize();
 }
 
 function onSuccessfulDeregistration() {
 
-  var basicConf = {
-    message: "Deregistration Successful",
-    alertType: constants.ALERT_TYPE_INFO,
-    alertTitle: "",
-    yesLabel: "OK",
-    noLabel: "",
-    alertHandler: null
-  }
-  var pspConf = {}
-  deregidterkpnspushservice();
+    var basicConf = {
+        message: "Deregistration Successful",
+        alertType: constants.ALERT_TYPE_INFO,
+        alertTitle: "",
+        yesLabel: "OK",
+        noLabel: "",
+        alertHandler: null
+    }
+    var pspConf = {}
+    deregidterkpnspushservice();
 
-  //var infoAlert = kony.ui.Alert(basicConf,pspConf);
-  kony.print("Successful deregistration");
+    //var infoAlert = kony.ui.Alert(basicConf,pspConf);
+    kony.print("Successful deregistration");
 
 }
 
 function onFailureDeregistration(payload) {
 
-  var basicConf = {
-    message: "Deregistration Failure" + payload,
-    alertType: constants.ALERT_TYPE_INFO,
-    alertTitle: "",
-    yesLabel: "OK",
-    noLabel: "",
-    alertHandler: null
-  }
-  var pspConf = {}
-  //var infoAlert = kony.ui.Alert(basicConf,pspConf);
-  kony.print("UnSuccessful deregistration");
+    var basicConf = {
+        message: "Deregistration Failure" + payload,
+        alertType: constants.ALERT_TYPE_INFO,
+        alertTitle: "",
+        yesLabel: "OK",
+        noLabel: "",
+        alertHandler: null
+    };
+    var pspConf = {};
+    //var infoAlert = kony.ui.Alert(basicConf,pspConf);
+    kony.print("UnSuccessful deregistration");
 }
 
 
 function subscribeKPNS() {
-  try {
-    kony.print("in subscribe " + gPushRegId);
-    if (gPushRegId == null || gPushRegId == "") {
-      alert('redId is null');
-      return;
+    try {
+        kony.print("in subscribe " + gPushRegId);
+        if (gPushRegId === null || gPushRegId === "") {
+            alert('redId is null');
+            return;
+        }
+        var appID = "20757-7758448819";
+        var ufid = "rahul.asthana@kony.com";
+        var myhttpheaders = {
+            "Content-Type": "application/json"
+        };
+        var ostype = "android" == kony.os.deviceInfo().name ? "androidgcm" : "iphone";
+        var deviceID = kony.os.deviceInfo().deviceid;
+        //#ifdef ipad
+        deviceID = kony.os.deviceInfo().identifierForVendor;
+        //#endif
+        //#ifdef iphone
+        deviceID = kony.os.deviceInfo().identifierForVendor;
+        //#endif
+
+
+        var sub = {
+            "sid": gPushRegId,
+            "appId": appID,
+            "ufid": ufid, //Need to change as per the business logic.
+            "deviceName": ostype + "_MM",
+            "osType": ostype,
+            "deviceId": deviceID
+        };
+        var inp = {
+            "subscriptionService": {
+                "subscribe": sub
+            }
+        };
+        kony.print("subscribeKPNS inputs::" + JSON.stringify(inp));
+        var paramTab = {
+            postdata: JSON.stringify(inp),
+            httpheaders: myhttpheaders
+        };
+        kony.net.invokeServiceAsync("https://novartis-eu-dev.messaging.konycloud.com/subscription", paramTab, registerAppWithKPNS_CallBack);
+    } catch (e) {
+        kony.print("Error in subscribeKPNS::" + e);
     }
-    var appID = "20757-7758448819";
-    var ufid = "rahul.asthana@kony.com";
-    var myhttpheaders = {
-      "Content-Type": "application/json"
-    };
-    var ostype = "android" == kony.os.deviceInfo().name ? "androidgcm" : "iphone";
-    var deviceID = kony.os.deviceInfo().deviceid;
-    //#ifdef ipad
-    deviceID = kony.os.deviceInfo().identifierForVendor;
-    //#endif
-    //#ifdef iphone
-    deviceID = kony.os.deviceInfo().identifierForVendor;
-    //#endif
-
-
-    var sub = {
-      "sid": gPushRegId,
-      "appId": appID,
-      "ufid": ufid, //Need to change as per the business logic.
-      "deviceName": ostype + "_MM",
-      "osType": ostype,
-      "deviceId": deviceID
-    };
-    var inp = {
-      "subscriptionService": {
-        "subscribe": sub
-      }
-    };
-    kony.print("subscribeKPNS inputs::" + JSON.stringify(inp));
-    var paramTab = {
-      postdata: JSON.stringify(inp),
-      httpheaders: myhttpheaders
-    };
-    kony.net.invokeServiceAsync("https://novartis-eu-dev.messaging.konycloud.com/subscription", paramTab, registerAppWithKPNS_CallBack);
-  } catch (e) {
-    kony.print("Error in subscribeKPNS::" + e);
-  }
 }
 
 function registerAppWithKPNS_CallBack(status, response) {
-  try {
-    kony.print("inside registerAppWithKPNS_CallBack   status is::" + status + "and response::" + JSON.stringify(response));
-    if (status == 400) {
-      kony.print("resutl is" + response["subscriptionResponse"]);
-      var resutl = response["subscriptionResponse"];
-      if (resutl["ksid"] != null) {
-        var ksid = resutl["ksid"]; //////Added for CSFR Token ID/////////
-      }
-      if (resutl["statusCode"] == 200 && resutl["ksid"] != -1) {
-        // store the ksid in local storage 
-        kony.store.setItem("ksid", resutl["ksid"]);
-      }
+    try {
+        kony.print("inside registerAppWithKPNS_CallBack   status is::" + status + "and response::" + JSON.stringify(response));
+        if (status == 400) {
+            kony.print("resutl is" + response["subscriptionResponse"]);
+            var resutl = response["subscriptionResponse"];
+            if (resutl["ksid"] !== null) {
+                var ksid = resutl["ksid"]; //////Added for CSFR Token ID/////////
+            }
+            if (resutl["statusCode"] == 200 && resutl["ksid"] != -1) {
+                // store the ksid in local storage 
+                kony.store.setItem("ksid", resutl["ksid"]);
+            }
+        }
+    } catch (e) {
+        kony.print("Error in registerAppWithKPNS_CallBack::" + e);
     }
-  } catch (e) {
-    kony.print("Error in registerAppWithKPNS_CallBack::" + e);
-  }
 }
--- modules/mdlPush.js
+++ modules/mdlPush.js
@@ -1,173 +1,59 @@
-//Type your code here
-//Type your code here
-mobileFabricConfiguration = 
-  {
-  appKey:"f872e665079e2e588156aa46b48a284e", 
-  appSecret:"7c1c65441ef0f2963ef1ec154c2830b5", 
-  serviceURL:"https://100011509.auth.konycloud.com/appconfig",
-  identityServices: 
-  [
-    {
-      service:"userstore",
-      username:"Novartis.Europe@kony.com",
-      password: "Default@1234"
-    },
-    {
-      service:"FacebookIdentity"
-    }
-  ],
-  konysdkObject: null,
-  authClient: null,
-  integrationServices:[{
-    service: "KonyAccounts",
-    operations: ["accountLogin"]	
-  },{
-    service: "PushOrchestration",
-    operations: ["PushBroadCastMsg"]
-  }],
-  integrationObj: null,
-  isKonySDKObjectInitialized:false,
-  isMFAuthenticated: false
-};
-
-function initializeMobileFabric()
-{
-  kony.print (" ********** Entering into initializeMobileFabric ********** ");
-  if (kony.net.isNetworkAvailable(constants.NETWORK_TYPE_ANY))
-  {
-    kony.application.showLoadingScreen("loadskin","Initializing",constants.LOADING_SCREEN_POSITION_FULL_SCREEN , true,true,{enableMenuKey:true,enableBackKey:true, progressIndicatorColor : "ffffff77"});
-    mobileFabricConfiguration.konysdkObject = new kony.sdk();
-    mobileFabricConfiguration.konysdkObject.init(mobileFabricConfiguration.appKey,mobileFabricConfiguration.appSecret,mobileFabricConfiguration.serviceURL,initializeMobileFabricSuccess,initializeMobileFabricFailure);  
-  }
-  else
-    alert ("Network unavailable. Please check your network settings. ");
-  kony.print (" ********** Exiting out of initializeMobileFabric ********** ");
-}
-
-function initializeMobileFabricSuccess(response)
-{
-  kony.print (" ********** Entering into initializeMobileFabricSuccess ********** ");
-  kony.print (" ********** Success initializeMobileFabricSuccess response : " + JSON.stringify(response) + " ********** ");
-  mobileFabricConfiguration.isKonySDKObjectInitialized=true;
-  kony.application.dismissLoadingScreen();
-  //authenticateMFUsingUserStore();
-  kony.print (" ********** Exiting out of initializeMobileFabricSuccess ********** ");
-}
-
-function initializeMobileFabricFailure(error)
-{
-  kony.print (" ********** Entering into initializeMobileFabricFailure ********** ");
-  kony.print (" ********** Failure in initializeMobileFabric: " + JSON.stringify(error) + " ********** ");
-  kony.application.dismissLoadingScreen();
-  alert (" Unable to initialize the application. Please try again. ");
-  kony.print (" ********** Exiting out of initializeMobileFabricFailure ********** ");
-}
-
-function authenticateMFUsingUserStore(){
-  kony.print (" ********** Entering into authenticateMFUsingUserStore ********** ");
-  kony.application.showLoadingScreen("loadskin","Fetching news !!!",constants.LOADING_SCREEN_POSITION_FULL_SCREEN , false,true,{enableMenuKey:true,enableBackKey:true, progressIndicatorColor : "ffffff77"});
-  mobileFabricConfiguration.authClient = mobileFabricConfiguration.konysdkObject.getIdentityService(mobileFabricConfiguration.identityServices[0].service);
-  var authParams = {"userid": frmLogin.txtUsername.text, 
-                    "password": frmLogin.txtPwd.text};
-  mobileFabricConfiguration.authClient.login(authParams, loginMFSuccess, loginMFFailure);
-  kony.print (" ********** Exiting out of authenticateMFUsingUserStore ********** ");
-}
-
-function loginMFSuccess(response){
-  mobileFabricConfiguration.isMFAuthenticated = true;
-  kony.application.dismissLoadingScreen();
-  frmMain.show();
-  frmMain.lblMail.text = frmLogin.txtUsername.text;
-}
-
-function loginMFFailure(error)
-{
-  kony.application.dismissLoadingScreen();
-  alert (" Unable to authenticate to Server, Login failed. Please try again. ");
-}
-
+/**
+ * Purpose: This function to claims token to pass composite service to push the message.
+ * 
+ */
 function getClaimToken() {
-  if (kony.net.isNetworkAvailable(constants.NETWORK_TYPE_ANY)) {
-      kony.application.showLoadingScreen("", "", constants.LOADING_SCREEN_POSITION_ONLY_CENTER, true, true, null);
-
-    mobileFabricConfiguration.integrationObj = mobileFabricConfiguration.konysdkObject.getIntegrationService(mobileFabricConfiguration.integrationServices[0].service);
-
-    var operationName = mobileFabricConfiguration.integrationServices[0].operations[0];
-
-    var headers = {"Content-Type": "application/json"};
-    var data = {
-      "userid": "raju.balugu@kony.com",
-      "password": "May@2017~1"
-    };
-
-    mobileFabricConfiguration.integrationObj.invokeOperation(operationName, headers, data, getClaimTokenSuccessCallback, getClaimTokenErrorCallback);
-    
-
-  }
-  else
-  {
-    alert("Network unavailable. Please check your network settings. ");
-  }
+    if (kony.net.isNetworkAvailable(constants.NETWORK_TYPE_ANY)) {
+        kony.application.showLoadingScreen("", "", constants.LOADING_SCREEN_POSITION_ONLY_CENTER, true, true, null);
+        mobileFabricConfiguration.integrationObj = mobileFabricConfiguration.konysdkObject.getIntegrationService(mobileFabricConfiguration.integrationServices[0].service);
+        var operationName = mobileFabricConfiguration.integrationServices[0].operations[0];
+        var headers = {
+            "Content-Type": "application/json"
+        };
+        mobileFabricConfiguration.integrationObj.invokeOperation(operationName, headers, getClaimTokenSuccessCallback, getClaimTokenErrorCallback);
+    } else {
+        alert("Network unavailable. Please check your network settings. ");
+    }
 }
 
-function getClaimTokenSuccessCallback(response){
-  kony.application.dismissLoadingScreen();
-  alert("Messages successfully sent to all subscribed devices");
+/**
+ * Purpose: Success callback of getClaimToken.
+ * 
+ */
+
+function getClaimTokenSuccessCallback(response) {
+    kony.application.dismissLoadingScreen();
+    alert("Messages successfully sent to all subscribed devices");
 }
 
-function getClaimTokenErrorCallback(response){
-  kony.application.dismissLoadingScreen();
-  alert("Failed to send message.Please try again.");
+/**
+ * Purpose: Failure callback of getClaimToken.
+ * 
+ */
+
+function getClaimTokenErrorCallback(response) {
+    kony.application.dismissLoadingScreen();
+    alert("Failed to send message.Please try again.");
 }
 
-function pushMessage(text){
-  if (kony.net.isNetworkAvailable(constants.NETWORK_TYPE_ANY)) {
-    kony.application.showLoadingScreen("", "", constants.LOADING_SCREEN_POSITION_ONLY_CENTER, true, true, null);
+/**
+ * Purpose: This function to push the message using integration service.
+ * 
+ */
 
-    mobileFabricConfiguration.integrationObj = mobileFabricConfiguration.konysdkObject.getIntegrationService(mobileFabricConfiguration.integrationServices[1].service);
-
-    var operationName = mobileFabricConfiguration.integrationServices[1].operations[0];
-    var headers = {
-      "X-Kony-Authorization":""
-    }; 
-    var data = {
-      "PMsg":text
-    };
-    mobileFabricConfiguration.integrationObj.invokeOperation(operationName, headers, data, getClaimTokenSuccessCallback, getClaimTokenErrorCallback);
-  }
-  else
-  {
-    alert("Network unavailable. Please check your network settings. ");
-  }
+function pushMessage(text) {
+    if (kony.net.isNetworkAvailable(constants.NETWORK_TYPE_ANY)) {
+        kony.application.showLoadingScreen("", "", constants.LOADING_SCREEN_POSITION_ONLY_CENTER, true, true, null);
+        mobileFabricConfiguration.integrationObj = mobileFabricConfiguration.konysdkObject.getIntegrationService(mobileFabricConfiguration.integrationServices[1].service);
+        var operationName = mobileFabricConfiguration.integrationServices[1].operations[0];
+        var headers = {
+            "X-Kony-Authorization": ""
+        };
+        var data = {
+            "PMsg": text
+        };
+        mobileFabricConfiguration.integrationObj.invokeOperation(operationName, headers, data, getClaimTokenSuccessCallback, getClaimTokenErrorCallback);
+    } else {
+        alert("Network unavailable. Please check your network settings. ");
+    }
 }
-
-function addUser(){
-  if (kony.net.isNetworkAvailable(constants.NETWORK_TYPE_ANY)) {
-    kony.application.showLoadingScreen("", "", constants.LOADING_SCREEN_POSITION_ONLY_CENTER, true, true, null);
-
-    mobileFabricConfiguration.integrationObj = mobileFabricConfiguration.konysdkObject.getIntegrationService("Store");
-
-    var operationName = "CreateUser";
-    var headers = {
-      "X-Kony-Authorization":""
-    }; 
-    var data = {
-      "userid":"test.rahul@kony.com",
-      "password":"text1234"
-    };
-    mobileFabricConfiguration.integrationObj.invokeOperation(operationName, headers, data, addUserSuccessCallBack, addUserErrorCallBack);
-  }
-  else
-  {
-    alert("Network unavailable. Please check your network settings. ");
-  }
-}
-
-function addUserSuccessCallBack(response){
-  alert("Success " + JSON.stringify(response))
-}
-
-function addUserErrorCallBack(response){
-  alert("Error " + JSON.stringify(response))
-}
-
